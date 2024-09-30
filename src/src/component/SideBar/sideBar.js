e9e39d7454134cf8a1ad4f3876dcfdd1cdd845ca
LaiQuangSon <152257579+laisonhehe@users.noreply.github.com>
2024-09-30 16:50:52 +0700

    Add files via upload

---
+++ src/src/component/SideBar/sideBar.js
@@ -0,0 +1,14 @@
+export default function header(){
+    return(
+        <header>
+        <h2 class="logo">Logo</h2>
+        <nav class="navigation">
+            <a href="#">1</a>
+            <a href="#">2</a>
+            <a href="#">3</a>
+            <a href="#">4</a>
+            <button class="btnLogin-popup">Login</button>
+        </nav>
+    </header>
+    )
+}
---
+++ src/src/view/register/registerUser.js
@@ -0,0 +1,36 @@
+function registerUser() {
+    return(
+        <div class="form-box register-2">
+            <h2>Đăng kí</h2>
+            <form>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="person-outline"></ion-icon></span>
+                    <input type="Username" required placeholder="Tên đăng nhập"/>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="lock-closed-outline"></ion-icon></span>
+                    <input type="password" required placeholder="Mật khẩu"/>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="mail-outline"></ion-icon></span>
+                    <input type="email" required placeholder="Email"/>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="call-outline"></ion-icon></span>
+                    <input type="Phone number" required placeholder="Số điện thoại"/>
+                </div>
+                <div class="input-box">
+                    <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
+                    <input type="Address" required placeholder="Địa chỉ"/>
+                </div>
+                <div class="input-box">
+                    <input type="date" id="dob" name="dob" required/>
+                </div>
+                <div>
+                    <button type="submit" class="btn">Đăng kí</button>
+                </div>
+            </form>
+        </div>
+    );
+}
+export default registerUser;
